aff1c7038f188e96287df13ea26358045b8f72e5
caidy <vcsdk2468@gmail.com>
2022-05-06 00:34:35 +0800

    modify

--- cruder-apt/src/test/resources/EntityL2.java
+++ cruder-apt/src/test/resources/EntityL2.java
@@ -4,6 +4,7 @@ package test;
 public class EntityL2<T> extends EntityL1<T> {
     private String field1;
     private int field2;
+    private T field3;
 
     public String getField1() {
         return field1;
@@ -11,5 +12,21 @@ public class EntityL2<T> extends EntityL1<T> {
 
     public void setField1(String field1) {
         this.field1 = field1;
+    }
+
+    public int getField2() {
+        return field2;
+    }
+
+    public void setField2(int field2) {
+        this.field2 = field2;
+    }
+
+    public T getField3() {
+        return field3;
+    }
+
+    public void setField3(T field3) {
+        this.field3 = field3;
     }
 }
