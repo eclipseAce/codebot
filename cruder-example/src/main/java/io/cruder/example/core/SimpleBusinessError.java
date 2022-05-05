adf930cde77ad5f5c172e5ca5f9c5d7421402858
caidy <vcsdk2468@gmail.com>
2022-05-05 18:19:07 +0800

    modify

--- cruder-example/src/main/java/io/cruder/example/core/SimpleBusinessError.java
+++
@@ -1,9 +0,0 @@
-package io.cruder.example.core;
-
-import lombok.Value;
-
-@Value
-public class SimpleBusinessError implements BusinessError {
-    private final String errorCode;
-    private final String errorMessage;
-}
