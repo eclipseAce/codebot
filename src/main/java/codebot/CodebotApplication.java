ab197e83b52a9aaba22f1c9a3a02919a6619cccc
caidy <vcsdk2468@gmail.com>
2022-06-28 15:44:33 +0800

    change to spring boot

---
+++ src/main/java/codebot/CodebotApplication.java
@@ -0,0 +1,63 @@
+package codebot;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import spoon.MavenLauncher;
+import spoon.reflect.CtModel;
+import spoon.reflect.declaration.CtPackage;
+import spoon.reflect.declaration.CtType;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@SpringBootApplication
+public class CodebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebotApplication.class, args);
+	}
+
+	private static void loadProject() throws GitAPIException {
+		Path originFolder = Paths.get("D:\\repo\\projectbasic");
+		Path targetFolder = Paths.get("D:\\codebot\\projectbasic");
+		setupProjectFolder(originFolder, targetFolder, "codebot");
+
+		MavenLauncher launcher = new MavenLauncher(
+				targetFolder.toString(),
+				MavenLauncher.SOURCE_TYPE.APP_SOURCE
+		);
+		launcher.buildModel();
+		CtModel model = launcher.getModel();
+
+		// list all packages of the model
+		for (CtPackage p : model.getAllPackages()) {
+			System.out.println("package: " + p.getQualifiedName());
+		}
+		// list all classes of the model
+		for (CtType<?> s : model.getAllTypes()) {
+			System.out.println("class: " + s.getQualifiedName());
+		}
+	}
+
+	private static void setupProjectFolder(Path origin, Path target, String branchName)
+			throws GitAPIException {
+
+		try (Git git = Git.cloneRepository()
+				.setURI(origin.toUri().toString())
+				.setDirectory(target.toFile())
+				.call()) {
+			if (git.branchList().call().stream().noneMatch(it -> it.getName().equals(branchName))) {
+				git.branchCreate()
+						.setName(branchName)
+						.setStartPoint("master")
+						.call();
+			}
+			git.checkout()
+					.setName(branchName)
+					.call();
+		}
+	}
+
+}
